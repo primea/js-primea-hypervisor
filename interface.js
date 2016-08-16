953c34e7f7eb6e1ff41fa8f1cd161a1f62909b34
Alex Beregszaszi <alex@rtfs.hu>
2016-08-16 23:41:03 +0100

    Interface: change internal getBalance to work with U256 and not ArrayBuffer

--- interface.js
+++ interface.js
@@ -95,7 +95,7 @@ module.exports = class Interface {
     const address = new Address(this.getMemory(addressOffset, constants.ADDRESS_SIZE_BYTES))
     // call the parent contract and ask for the balance of one of its child contracts
     const balance = this.environment.parent.environment.getBalance(address)
-    this.setMemory(offset, constants.BALANCE_SIZE_BYTES, balance)
+    this.setMemory(offset, constants.BALANCE_SIZE_BYTES, balance.toBuffer(constants.BALANCE_SIZE_BYTES))
   }
 
   /**

6c29aaeb576cb5fe8d11d3713076e86bb208b8e7
wanderer <mjbecze@gmail.com>
2016-08-17 10:39:33 -0400

    simplifies address.js by extending Buffer directly

--- address.js
+++ address.js
@@ -1,38 +1,27 @@
-const ethUtils = require('ethereumjs-util')
+const ethUtil = require('ethereumjs-util')
 
-module.exports = class Address extends Uint8Array {
+module.exports = class Address extends Buffer {
   constructor (value) {
-    super(value)
-    // Special case: duplicate
-    if (value instanceof Address) {
-      this._value = new Buffer(value._value)
-      return
-    }
-
-    if (value instanceof Uint8Array) {
-      this._value = new Buffer(value)
+    if (value instanceof Address || value instanceof Uint8Array) {
+      super(value)
     } else if (typeof value !== 'string') {
       throw new Error('Invalid input to address')
-    } else if (!ethUtils.isHexPrefixed(value)) {
+    } else if (!ethUtil.isHexPrefixed(value)) {
       throw new Error('Invalid address format')
     } else {
-      this._value = new Buffer(ethUtils.stripHexPrefix(value), 'hex')
+      super(ethUtil.stripHexPrefix(value), 'hex')
     }
 
-    if (this._value.length !== 20) {
+    if (this.length !== 20) {
       throw new Error('Invalid address length')
     }
   }
 
-  toBuffer () {
-    return this._value
-  }
-
   toString () {
-    return '0x' + this._value.toString('hex')
+    return '0x' + this.toString('hex')
   }
 
   isZero () {
-    return this._value.equals(ethUtils.zeros(20))
+    return this.every((el) => el === 0)
   }
 }

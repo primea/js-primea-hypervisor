59095b0502d442d94aafa6267d3d4e3cb01d7ba6
Alex Beregszaszi <alex@rtfs.hu>
2016-08-23 02:11:29 +0100

    Tests: use two-level hierarchy for block level values

--- testEnvironment.js
+++ testEnvironment.js
@@ -2,6 +2,7 @@ const Environment = require('./environment.js')
 const U256 = require('./u256.js')
 const Address = require('./address.js')
 const Block = require('./block.js')
+const ethUtil = require('ethereumjs-util')
 
 module.exports = class TestEnvironment extends Environment {
   constructor (data) {
@@ -50,30 +51,32 @@ module.exports = class TestEnvironment extends Environment {
       self.gasLeft = data.gasLeft
     }
 
-    let block = {}
+    if (data.block) {
+      let block = {}
 
-    if (data.blockNumber) {
-      block.number = data.blockNumber
-    }
+      if (data.block.blockNumber) {
+        block.number = ethUtil.toBuffer(data.block.blockNumber)
+      }
 
-    if (data.gasLimit) {
-      block.gasLimit = data.gasLimit
-    }
+      if (data.block.gasLimit) {
+        block.gasLimit = ethUtil.toBuffer(data.block.gasLimit)
+      }
 
-    if (data.difficulty) {
-      block.difficulty = adta.difficulty
-    }
+      if (data.block.difficulty) {
+        block.difficulty = ethUtil.toBuffer(data.block.difficulty)
+      }
 
-    if (data.timestamp) {
-      block.timestamp = data.timestamp
-    }
+      if (data.block.timestamp) {
+        block.timestamp = ethUtil.toBuffer(data.block.timestam)
+      }
 
-    if (data.coinbase) {
-      block.coinbase = data.coinbase
-    }
+      if (data.block.coinbase) {
+        block.coinbase = ethUtil.toBuffer(data.block.coinbase)
+      }
 
-    if (Object.keys(block).length > 0) {
-      self.block = new Block({ header: block, transactions: [], uncleHeaders: [] })
+      if (Object.keys(block).length > 0) {
+        self.block = new Block({ header: block, transactions: [], uncleHeaders: [] })
+      }
     }
   }
 }

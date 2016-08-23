aa7f348ee71a97e479efcc2d058b302bcf683a0d
Alex Beregszaszi <alex@rtfs.hu>
2016-08-23 01:51:46 +0100

    Precompile: add comments about implementation

--- precompile.js
+++ precompile.js
@@ -1,6 +1,10 @@
+// const evm2wasm = require('evm2wasm')
+// const metering = require('wasm-metering')
+
 module.exports.meteringInjector = function (call) {
   console.log('Executing metering injector')
   return {
+    // returnValue: metering.injectWAST(call.data, 2).slice(0)
     returnValue: call.data.slice(0)
   }
 }
@@ -8,6 +12,7 @@ module.exports.meteringInjector = function (call) {
 module.exports.transcompiler = function (call) {
   console.log('Executing transcompiler')
   return {
+    // returnValue: evm2wasm.compileEVM(call.data).slice(0)
     returnValue: call.data.slice(0)
   }
 }

e7e36382dc9e3373aad516a7c37df5e4fa7554a6
Alex Beregszaszi <alex@rtfs.hu>
2016-08-27 00:41:38 +0100

    Kernel: use Frontier-style address generation for accounts

--- index.js
+++ index.js
@@ -135,7 +135,12 @@ module.exports = class Kernel {
       code = this.callHandler({ from: Address.zero(), to: meteringContract, gasLimit: 0, value: new U256(0), data: code }).returnValue
     }
 
-    let address = Utils.newAccountAddress(create.from, code)
+    let account = this.environment.state.get(create.from.toString())
+    if (!account) {
+      throw new Error('Account not found: ' + create.from.toString())
+    }
+
+    let address = Utils.newAccountAddress(create.from, account.get('nonce'))
 
     this.environment.addAccount(address.toString(), {
       balance: create.value,

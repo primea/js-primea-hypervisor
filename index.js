277b81fa0326007d061917f6ea1efa15f8ad5392
wanderer <mjbecze@gmail.com>
2016-11-05 07:07:58 -0400

    docs

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-/**
- * This implements the Kernel
- * Kernels must implement two methods `codeHandler` and `callHandler` (and `linkHandler` for sharding)
- * The Kernel Contract handles the following
- * - Interprocess communications
- * - Intializing the VM and exposes ROM to it (codeHandler)
- * - Expose namespace which VM instance exists and Intializes the Environment (callHandler)
- * - Provides some built in contract (runTx, runBlock)
- * - Provides resource sharing and limiting via gas
- *
- *   All State should be stored in the Environment.
- */
-
 const Vertex = require('./deps/kernelVertex')
 // The Kernel Exposes this Interface to VM instances it makes
 const Interface = require('./interface.js')
 const InterfaceAPI = require('./interfaceAPI.js')
-
-// The Kernel Stores all of its state in the Environment. The Interface is used
-// to by the VM to retrive infromation from the Environment.
 const Environment = require('./environment.js')
+
 module.exports = class Kernel {
   constructor (opts = {}) {
     this.state = opts.state || new Vertex()
@@ -30,6 +15,9 @@ module.exports = class Kernel {
     this.imports = this.buildImports(opts.interfaces)
   }
 
+  /**
+   * Builds a import map with an array of given interfaces
+   */
   buildImports (interfaces = [Interface]) {
     return interfaces.reduce((obj, Interface) => {
       obj[Interface.name] = new Interface(this.interfaceAPI).exports
@@ -37,7 +25,11 @@ module.exports = class Kernel {
     }, {})
   }
 
-  // run the kernels code with a given enviroment
+  /**
+   * run the kernels code with a given enviroment
+   * The Kernel Stores all of its state in the Environment. The Interface is used
+   * to by the VM to retrive infromation from the Environment.
+   */
   async run (environment = new Environment({state: this.state}), imports = this.imports) {
     await this.interfaceAPI.run(environment, imports)
     return environment
